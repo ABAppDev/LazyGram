18eef7d6532f781462705b80533fd144b2764016
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2021-11-20 13:12:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter >
       <Navbar />
-      <Routes>
+      <Routes path="/" >
         <Route  path="/" element={<Home />} />
         <Route  path="/login" element={<Login />} />
         <Route  path="/signup" element={<Signup />} />
