e7039965df5cd04feae4042b3f7e9c9c030e610a
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2021-11-20 18:29:26 +0530

    update

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -2,7 +2,14 @@ import React from "react"
 
 const Login = () => {
     return (
-        <h6 className="log-greeting">Login to your LazyGram</h6>
+        <div>
+            <div class="card curve">
+                <h2>LazyGram</h2>
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button class="btn waves-effect waves-light curve" type="submit" name="action">Login</button>
+            </div>
+        </div>
     )
 }
 export default Login
