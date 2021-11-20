b383267e837710223016aff22076d0e55ea00095
ABHISHEK PRALHAD BHALERAO <abhishekbhalerao.appdev@gmail.com>
2021-11-20 12:37:59 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/screens/Home';
 import Profile from './components/screens/Profile';
@@ -9,7 +9,7 @@ import Signup from './components/screens/Signup';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.URL}>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/profile" element={<Profile />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
